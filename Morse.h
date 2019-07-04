21a33b5702a385e3c4ce03434946f7e7f27f5d8c
ikunkun8 <52461598+ikunkun8@users.noreply.github.com>
2019-07-04 10:29:38 +0800

    Add files via upload

---
+++ Morse.h
@@ -0,0 +1,15 @@
+＃IFNDEF _MORSE_H
+＃定义 _MORSE_H
+类 莫尔斯
+{
+  上市：
+    莫尔斯（int pin）;
+    void  dot（）;
+    void  dash（）;
+    void  c_space（）;
+    void  w_space（）;
+  私人的：
+    int _pin;
+    int _dottime;
+};
+＃ENDIF  / * _MORSE_H * /
